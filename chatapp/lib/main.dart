2f9a79390b7ff593d57b70fbeabb25bd1d948d1b
02074 <benjamin.anumel@yahoo.com>
2021-12-01 15:26:32 +0000

    before extracting

--- chatapp/lib/main.dart
+++ chatapp/lib/main.dart
@@ -34,7 +34,28 @@ class ChatApp extends StatelessWidget {
           Icon(Icons.search), Icon(Icons.more_horiz), Icon(Icons.camera)
         ],
       ),
-    body: const Center(child: CircleAvatar(radius: 70, backgroundColor: Colors.green, backgroundImage: NetworkImage("https://images.unsplash.com/photo-1637419980533-1119de04312f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"), ),
+    body: Card(elevation: 10,
+    color:  Colors.blueAccent,
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListTile(
+        leading: const CircleAvatar(backgroundImage: NetworkImage("https://images.unsplash.com/flagged/photo-1573603867003-89f5fd7a7576?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=446&q=80"),
+        radius: 30,
+        
+        ),
+        title:  Text("Ben"),
+        subtitle:Row(
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            const Icon(Icons.done_all),
+             Text("message")
+          ],
+        ),
+        trailing:  Text("2:00pm",
+      ),
+    ),
+    
+    )
     )
     );
   }
